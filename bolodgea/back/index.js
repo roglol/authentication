4068007fe1801192ff4e5749c559b8ed74bc4895
roglol <gibradze.gorda@yahoo.com>
2018-12-09 12:31:51 +0400

    vaime

---
+++ bolodgea/back/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const cors = require('cors')
+const crypto = require('crypto')
+
+const app = express()
+
+app.use(cors('*'))
+
+app.get('/', (req,res) =>{
+    res.send("Hello")
+})
+
+app.listen(5000, () => {
+    console.log(`Port - 5000`)
+})
